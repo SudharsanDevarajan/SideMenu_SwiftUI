8a5083829c6cd8f6eeb56d6eed0113d530228fe5
sudharsan <sudharsan.devarajan@htcinc.com>
2022-11-23 15:15:27 +0530

    Initial Commit

---
+++ SideMenu_SwiftUI/SideMenu_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenu_SwiftUIApp.swift
+//  SideMenu_SwiftUI
+//
+//  Created by htcuser on 23/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SideMenu_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
